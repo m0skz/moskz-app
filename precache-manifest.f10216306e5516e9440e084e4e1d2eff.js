3579b2417145f73d5ff90efe7b6110f4242a9c2c
moskz <m0skz@users.noreply.github.com>
2022-02-12 23:14:14 +0100

    Updates

---
+++ precache-manifest.f10216306e5516e9440e084e4e1d2eff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f67c64e444f746f9a1e5fea3627b8aa",
+    "url": "/moskz-app/index.html"
+  },
+  {
+    "revision": "f20428a8558be1c48d1b",
+    "url": "/moskz-app/static/css/main.caef1bc2.chunk.css"
+  },
+  {
+    "revision": "df359363f1e67bfe9c14",
+    "url": "/moskz-app/static/js/2.1642ddda.chunk.js"
+  },
+  {
+    "revision": "5097f7748032a6907a24",
+    "url": "/moskz-app/static/js/3.340461a2.chunk.js"
+  },
+  {
+    "revision": "f20428a8558be1c48d1b",
+    "url": "/moskz-app/static/js/main.908c6ca2.chunk.js"
+  },
+  {
+    "revision": "ceca5a19d26939385895",
+    "url": "/moskz-app/static/js/runtime~main.2c1d47dc.js"
+  },
+  {
+    "revision": "1b8d2da6ec9d0972ea43fea0f3caab01",
+    "url": "/moskz-app/static/media/logo.1b8d2da6.png"
+  }
+]);
